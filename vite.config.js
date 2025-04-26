c89b7c416549c124154eaa179f4af4c0162773d5
Danny Pocus <dpocus21@gmail.com>
2025-04-25 20:06:25 -0700

    Update base URL in vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: '/danastasia-zelda/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
